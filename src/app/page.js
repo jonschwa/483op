54666874711e2ec2296ddeb68d3fd15a19727dab
Jon Schwartz <jalexan@gmail.com>
2023-11-04 01:52:57 -0400

    start simple

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
           <li>sakshi.agarwal2@ae-re.com</li>
         </ul>
         <p>Please provide your name and apartment number. Don&apos;t worry! You will have a chance to edit the email before sending!</p>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div>
       <form class="w-full max-w-sm">
         <div class="md:flex md:items-center mb-6">
           <div class="md:w-1/3">
@@ -35,31 +35,22 @@ export default function Home() {
             <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-apartment-number" type="text" placeholder="1A"/>
           </div>
         </div>
-        <div class="md:flex md:items-center mb-6">
-          <div class="md:w-1/3">
-            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
-              Apartment Number
-            </label>
-          </div>
-          <div class="md:w-2/3">
-            <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-apartment-number" type="text" placeholder="1A"/>
-          </div>
-        </div>
-        <div class="md:flex md:items-center mb-6 text-left">
+        <div>
+        <div>
         <p>Hello,</p>
+        </div>
         <p>I am a tenant of 483 Ocean Parkway, and I am extremely disappointed that the buzzer on our front door has been broken for so long. Not only is this inconvenient for deliveries and guests, but it is also a safety and security concern. I pay good money every month to live here, and the lack of communication and action are completely unacceptable.</p>
         <p>On behalf of myself and all the other tenants in the building, I demand a response to this email and an acknowledgement of this problem along with a reasonable estimate of when it will be resolved.</p>
         <p>If this matter continues to go unaddressed, I will be completely within my right as a tenant to...</p>
         <p>I look forward to your timely response.</p>
         <p>Thank you,</p>
         <p>Tenant Name, Apt #</p>
-
         </div>
-        <div class="md:flex md:items-center">
+        <div class="md:flex md:items-center ">
           <div class="md:w-1/3"></div>
-          <div class="md:w-2/3">
+          <div class="md:w-2/3 p-2">
             <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-              Sign Up
+              Send
             </button>
           </div>
         </div>
